26a34f14a2b4aa360dda786fda3faa6e3b68b21a
ElZidane123 <elzidaneardyansyah265@gmail.com>
2025-01-27 21:38:30 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded",function(){
+
+    new TypeIt("#simpleUsage", {
+        strings: "",
+        speed: 100,
+        waitUntilVisible: true,
+    }).go();
+});
+
+document.addEventListener("DOMContentLoaded",function(){
+
+    new TypeIt("#usage", {
+        strings: "",
+        speed: 70,
+        waitUntilVisible: true,
+    }).go();
+});
